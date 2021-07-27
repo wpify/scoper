253ea79b6983ca346315c18886772cf5462a14a0
Václav Greif <info@vaclavgreif.cz>
2021-07-27 18:06:43 +0000

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -200,6 +200,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
 			'--working-dir'          => $path,
 			'--ignore-platform-reqs' => true,
 			'--optimize-autoloader'  => true,
+			'--no-cache'  => true,
 		) ), $output );
 	}
 }
