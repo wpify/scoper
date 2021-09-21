071461d45d98b5340cb7dc10b0d886f1654317d3
Daniel Mejta <daniel@mejta.net>
2021-09-21 15:06:43 +0200

    fixes

---
+++ symbols/plugin-update-checker.php
@@ -0,0 +1,1 @@
+<?php return array('Puc_v4_Factory','Puc_v4p11_Factory','Puc_v4p11_UpdateChecker','Puc_v4p11_UpgraderStatus','Puc_v4p11_Utils','Puc_v4p11_Autoloader','Puc_v4p11_InstalledPackage','Puc_v4p11_Metadata','Puc_v4p11_OAuthSignature','Puc_v4p11_Scheduler','Puc_v4p11_StateStore','Puc_v4p11_Update');
